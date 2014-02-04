65ed9fdd277018ce04e0fc61d4a3e1d597141d36
Arielle Adams <arielle919@gmail.com>
2014-02-04 09:16:25 -0600

    last update-rake clean

--- test/test_listing_task.rb
+++ test/test_listing_task.rb
@@ -7,12 +7,12 @@ def test_client_incomplete_task_list_returns_relevant_results
     sam_jones = Task.create(name: "Sam Jones", appointment: "none", task: "Sign Paper", taskCompleted: "no")
     tim_collins = Task.create(name: "Tim Collins", appointment: "none", task: "Sign Contract", taskCompleted: "no")
 
-    command = "./clienttracker 'task processing'"
+    command = "./clienttracker 'task incomplete'"
     expected = <<EOS.chomp
 Incomplete Task:
-#{sammy_cole.id} Sammy Cole Read Docs
-#{sam_jones.id} Sam Jones Sign Paper
-#{tim_collins.id} Tim Collins Sign Contract
+#{sammy_cole.id} Sammy Cole none Read Docs
+#{sam_jones.id} Sam Jones none Sign Paper
+#{tim_collins.id} Tim Collins none Sign Contract
 EOS
     assert_command_output expected, command
   end
